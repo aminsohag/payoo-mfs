eea29e33be379d7a01a2e53a1495bda966bc069c
aminsohag <aminsohag508@gmail.com>
2024-09-27 20:01:03 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,22 @@
+// console.log('button clicking file added');
+
+// document.getElementById('id').addEventListener('click',function(){})
+// document.getElementById('button-login')
+//     .addEventListener('click', function(){
+        
+//     })
+
+// search : form submit reloading the page
+
+// step 1 : set event handler
+document.getElementById('button-login')
+    .addEventListener('click', function(event){
+        // step 2 : prevent default beahavior (prevent reloading browser)
+        event.preventDefault(); // <------ Vejal to beginners
+       console.log('login button clicked');
+        
+       // step 3 : get the phone number
+       const phoneNumber = document.getElementById('phone-number').value;
+       console.log(phoneNumber);
+       
+    })
